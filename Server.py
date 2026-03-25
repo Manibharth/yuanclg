3304b21a8d04a7c5d0e4af067872d2216f46edcc
Mani bharathi <manibharathi2k6@gmail.com>
2026-03-25 20:55:34 +0530

    add flask deployment files

--- Server.py
+++ Server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 # Step 1: Import our auth functions
 from auth import register_user, login_user
@@ -64,5 +65,5 @@ def login():
 if __name__ == '__main__':
     # debug=True → auto-restarts server when you save changes
     # port=5000  → browser will call http://localhost:5000
-    print("🚀 Flask server running at http://localhost:5000")
-    app.run(debug=True, port=5000)
+    print("🚀 Flask server running at os.environ.get_PORT, 5000")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
